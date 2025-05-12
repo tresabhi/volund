9bbb89ba46cb8afe41b1f560c019e7dde6f681e5
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-05-11 23:34:15 -0500

    feat: macos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// TODO: remove this
 // #![allow(
 //   dead_code,
 //   unused_variables,
@@ -9,6 +10,7 @@ use anyhow::{anyhow, Result};
 use log::*;
 use vulkanalia::loader::{LibloadingLoader, LIBRARY};
 use vulkanalia::prelude::v1_0::*;
+use vulkanalia::vk::ExtExtendedDynamicState3Extension;
 // TODO: update to latest prelude
 use vulkanalia::window::{self as vk_window, get_required_instance_extensions};
 use vulkanalia::Version;
@@ -17,6 +19,8 @@ use winit::event::{Event, WindowEvent};
 use winit::event_loop::EventLoop;
 use winit::window::Icon;
 use winit::window::{Window, WindowBuilder}; // TODO: update winit to the latest version
+
+const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
 
 fn main() -> Result<()> {
   pretty_env_logger::init();
@@ -70,13 +74,26 @@ unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
     .engine_name(b"Apollo\0")
     .engine_version(vk::make_version(1, 0, 0))
     .api_version(vk::make_version(1, 1, 0));
-  let extensions = vk_window::get_required_instance_extensions(window)
+  let mut extensions = vk_window::get_required_instance_extensions(window)
     .iter()
     .map(|e| e.as_ptr())
     .collect::<Vec<_>>();
+  let flags = if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION {
+    info!("Enabling extensions for macOS portability");
+    extensions.push(
+      vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_EXTENSION
+        .name
+        .as_ptr(),
+    );
+    extensions.push(vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name.as_ptr());
+    vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+  } else {
+    vk::InstanceCreateFlags::empty()
+  };
   let info = vk::InstanceCreateInfo::builder()
     .application_info(&application_info)
-    .enabled_extension_names(&extensions);
+    .enabled_extension_names(&extensions)
+    .flags(flags);
 
   Ok(entry.create_instance(&info, None)?)
 }
