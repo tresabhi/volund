fb20ffb7cd396058e3936e27d99be06fc23c0f83
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-05-13 10:13:36 -0500

    feat: validation logging

--- src/main.rs
+++ src/main.rs
@@ -73,19 +73,17 @@ fn main() -> Result<()> {
   Ok(())
 }
 
-unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
+unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData) -> Result<Instance> {
   let application_info = vk::ApplicationInfo::builder()
     .application_name(b"Volund\0")
     .application_version(vk::make_version(1, 0, 0))
     .engine_name(b"Apollo\0")
     .engine_version(vk::make_version(1, 0, 0))
     .api_version(vk::make_version(1, 1, 0));
-
   let mut extensions = vk_window::get_required_instance_extensions(window)
     .iter()
     .map(|e| e.as_ptr())
     .collect::<Vec<_>>();
-
   let flags = if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION {
     info!("Enabling extensions for macOS portability");
     extensions.push(
@@ -98,15 +96,19 @@ unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
   } else {
     vk::InstanceCreateFlags::empty()
   };
-
   let available_layers = entry
     .enumerate_instance_layer_properties()?
     .iter()
     .map(|l| l.layer_name)
     .collect::<HashSet<_>>();
 
+  // TODO: merge these two if statements
   if VALIDATION_ENABLED && !available_layers.contains(&VALIDATION_LAYER) {
     return Err(anyhow!("Validation layer requested but not supported."));
+  }
+
+  if VALIDATION_ENABLED {
+    extensions.push(vk::EXT_DEBUG_UTILS_EXTENSION.name.as_ptr());
   }
 
   let layers = if VALIDATION_ENABLED {
@@ -114,28 +116,70 @@ unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
   } else {
     Vec::new()
   };
-
   let info = vk::InstanceCreateInfo::builder()
     .application_info(&application_info)
     .enabled_layer_names(&layers)
     .enabled_extension_names(&extensions)
     .flags(flags);
+  let instance = entry.create_instance(&info, None)?;
 
-  Ok(entry.create_instance(&info, None)?)
+  if VALIDATION_ENABLED {
+    let debug_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+      .message_severity(vk::DebugUtilsMessageSeverityFlagsEXT::all())
+      .message_type(
+        vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+          | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+          | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+      )
+      .user_callback(Some(debug_callback));
+
+    data.messenger = instance.create_debug_utils_messenger_ext(&debug_info, None)?;
+  }
+
+  Ok(instance)
+}
+
+extern "system" fn debug_callback(
+  severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+  type_: vk::DebugUtilsMessageTypeFlagsEXT,
+  data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+  _: *mut c_void,
+) -> vk::Bool32 {
+  let data = unsafe { *data };
+  let message = unsafe { CStr::from_ptr(data.message) }.to_string_lossy();
+
+  if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::ERROR {
+    error!("({:?}) {}", type_, message);
+  } else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::WARNING {
+    warn!("({:?}) {}", type_, message);
+  } else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::INFO {
+    debug!("({:?}) {}", type_, message);
+  } else {
+    trace!("({:?}) {}", type_, message);
+  }
+
+  vk::FALSE
 }
 
 #[derive(Clone, Debug)]
 struct App {
   entry: Entry,
   instance: Instance,
+  data: AppData,
 }
 
 impl App {
   unsafe fn create(window: &Window) -> Result<Self> {
     let loader = LibloadingLoader::new(LIBRARY)?;
     let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
-    let instance = create_instance(window, &entry)?;
-    Ok(Self { entry, instance })
+    let mut data = AppData::default();
+    let instance = create_instance(window, &entry, &mut data)?;
+
+    Ok(Self {
+      entry,
+      instance,
+      data,
+    })
   }
 
   /// Renders a frame for our Vulkan app.
@@ -145,10 +189,18 @@ impl App {
 
   /// Destroys our Vulkan app.
   unsafe fn destroy(&mut self) {
+    if VALIDATION_ENABLED {
+      self
+        .instance
+        .destroy_debug_utils_messenger_ext(self.data.messenger, None);
+    }
+
     self.instance.destroy_instance(None);
   }
 }
 
 /// The Vulkan handles and associated properties used by our Vulkan app.
 #[derive(Clone, Debug, Default)]
-struct AppData {}
+struct AppData {
+  messenger: vk::DebugUtilsMessengerEXT,
+}
