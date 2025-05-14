676532f85a588496298272937be3f06c47c11122
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-05-13 22:11:58 -0500

    feat: picking device

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@
 //   clippy::unnecessary_wraps
 // )]
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Ok, Result};
 use log::*;
 use std::collections::HashSet;
 use std::ffi::CStr;
 use std::os::raw::c_void;
+use thiserror::Error;
 use vulkanalia::loader::{LibloadingLoader, LIBRARY};
 use vulkanalia::prelude::v1_0::*; // TODO: update to latest prelude
 use vulkanalia::vk::{DebugUtilsMessageSeverityFlagsEXT, ExtExtendedDynamicState3Extension};
@@ -69,6 +70,84 @@ fn main() -> Result<()> {
       _ => {}
     }
   })?;
+
+  Ok(())
+}
+
+#[derive(Copy, Clone, Debug)]
+struct QueueFamilyIndices {
+  graphics: u32,
+}
+
+impl QueueFamilyIndices {
+  unsafe fn get(
+    instance: &Instance,
+    data: &AppData,
+    physical_device: vk::PhysicalDevice,
+  ) -> Result<Self> {
+    let properties = instance.get_physical_device_queue_family_properties(physical_device);
+    let graphics = properties
+      .iter()
+      .position(|p| p.queue_flags.contains(vk::QueueFlags::GRAPHICS))
+      .map(|i| i as u32);
+
+    if let Some(graphics) = graphics {
+      Ok(Self { graphics })
+    } else {
+      Err(anyhow!(SuitabilityError(
+        "Missing required queue families."
+      )))
+    }
+  }
+}
+
+#[derive(Debug, Error)]
+#[error("Missing {0}.")]
+pub struct SuitabilityError(pub &'static str);
+
+unsafe fn pick_physical_device(instance: &Instance, data: &mut AppData) -> Result<()> {
+  for physical_device in instance.enumerate_physical_devices()? {
+    let properties = instance.get_physical_device_properties(physical_device);
+
+    if let Err(error) = check_physical_device(instance, data, physical_device) {
+      // reconsider this
+      warn!(
+        "Skipping physical device (`{}`): {}",
+        properties.device_name, error
+      );
+    } else {
+      info!("Selected physical device (`{}`).", properties.device_name);
+
+      data.physical_device = physical_device;
+
+      return Ok(());
+    }
+  }
+
+  Err(anyhow!("Failed to find suitable physical device."))
+}
+
+unsafe fn check_physical_device(
+  instance: &Instance,
+  data: &AppData,
+  physical_device: vk::PhysicalDevice,
+) -> Result<()> {
+  let properties = instance.get_physical_device_properties(physical_device);
+  if properties.device_type != vk::PhysicalDeviceType::DISCRETE_GPU {
+    // TODO: reconsider this
+    return Err(anyhow!(SuitabilityError(
+      "Only discrete GPUs are supported."
+    )));
+  }
+
+  let features = instance.get_physical_device_features(physical_device);
+  if features.geometry_shader != vk::TRUE {
+    return Err(anyhow!(SuitabilityError(
+      "Missing geometry shader support."
+    )));
+  }
+
+  QueueFamilyIndices::get(instance, data, physical_device)?;
 
   Ok(())
 }
@@ -179,6 +258,8 @@ impl App {
     let mut data = AppData::default();
     let instance = create_instance(window, &entry, &mut data)?;
 
+    pick_physical_device(&instance, &mut data)?;
+
     Ok(Self {
       entry,
       instance,
@@ -207,4 +288,5 @@ impl App {
 #[derive(Clone, Debug, Default)]
 struct AppData {
   messenger: vk::DebugUtilsMessengerEXT,
+  physical_device: vk::PhysicalDevice,
 }
